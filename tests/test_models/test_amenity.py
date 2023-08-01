cc424f7c9948db0227a22f45da86cb0e198a2171
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-01 15:32:29 -0500

    test_amenity.py

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -6,7 +6,7 @@ from models.amenity import Amenity
 
 class test_Amenity(test_basemodel):
     """ """
-    
+
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
