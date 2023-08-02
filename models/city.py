3f9682b3e45e91e5e3d4787d383829ac4c2b2952
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-02 11:12:14 -0500

    Update city.py

--- models/city.py
+++ models/city.py
@@ -6,7 +6,7 @@ from models.base_model import BaseModel, Base
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = 'cities'
-    if: getenv('if getenv('HBNB_TYPE_STORAGE') == 'db':
+    if getenv('if getenv('HBNB_TYPE_STORAGE') == 'db':
         name = Column(String(128), nullable=False)
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     else:
