661e65ac1f8c97f98a7a719813697394b6c52ae8
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-02 11:37:59 -0500

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 """This module defines a base class for all models in our hbnb clone"""
 import uuid
+from os import getenv
 from datetime import datetime
+import models
+from sqlalchemy import Column, String, Integer, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
 Base = declarative_base()
 
 class BaseModel:
