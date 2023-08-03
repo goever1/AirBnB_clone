f9a3174e20eda7ebc765fae484ddf8790074278a
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-03 12:45:28 -0500

    reset file_storage.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,16 +8,9 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self, cls=None):
+    def all(self):
         """Returns a dictionary of models currently in storage"""
-        if cls is None:
-            return FileStorage.__objects
-        else:
-            dict = {}
-            for key, value in FileStorage.__objects.items():
-                if isinstance(value, cls):
-                    dict[key] = value
-        return dict
+        return FileStorage.__objects
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -52,19 +45,6 @@ class FileStorage:
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                    self.all()[key] = classes[val['__class__']](**val)
+                        self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
-
-    def delete(self, obj=None):
-        """To delete obj from __objects if it’s inside"""
-        if not obj:
-            return
-        key = "{}.{}".format(type(obj).__name__, obj.id)
-        if key in self.__objects:
-            del self.__objects[key]
-            self.save()
-
-    def close(self):
-        """Call reload() method for deserializing the JSON file to objects"""
-        self.reload()
