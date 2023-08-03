5e46a885c1c5e4d42174bd3e43680847a3a72d1b
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-03 10:47:50 -0500

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
-
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    name = Column(string(128), nullable=False)
+
+@property
+def cities(self):
+    from models import storage
+    from models.city import City
+    cities_list = []
+    for city in storage.all(city).values():
+        if city.state_id == seld.id:
+            cities_list.append(city)
+    return cities_list
