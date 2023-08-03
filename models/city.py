60600f1e50bdc35ab066038d8176b774e5e61599
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-03 12:12:50 -0500

    Update city.py

--- models/city.py
+++ models/city.py
@@ -7,4 +7,4 @@ class City(BaseModel):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    state_id = Column(String(60), nullable=False, Foreignkey("states.id")
+    state_id = Column(String(60), nullable=False, Foreignkey("states.id"))
