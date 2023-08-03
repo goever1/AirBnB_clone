28f18c5a24c34bf52397bb83240c2184cc65415d
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-03 09:55:17 -0500

    reset state.py

--- models/state.py
+++ models/state.py
@@ -1,18 +1,8 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from models.city import City
-import models
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
+from models.base_model import BaseModel
 
-class State(BaseModel, Base):
+
+class State(BaseModel):
     """ State class """
-    __tablename__ = 'states'
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        """ db ==>  means let's go for SQLAlchemy logic"""
-        name = Column(String(128), nullable=False)
-        cities = relationship('City', cascade='all, delete', backref='state')
-    else:
-        name = ""
+    name = ""
