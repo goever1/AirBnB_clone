d9c2850df96e6ead901455af792c27da7c02c0f8
gaburon <japo000@hotmail.com>
2023-08-03 16:11:36 -0500

    si

--- models/city.py
+++ models/city.py
@@ -2,9 +2,20 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
+from os import getenv
 
-class City(BaseModel, Base):
+env = getenv('HBNB_TYPE_STORAGE')
+
+
+class City(BaseModel, Base if (env == "db") else object):
     """ The city class, contains state ID and name """
-    __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    if env == "db":
+        __tablename__ = "cities"
+
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
+    else:
+        name = ""
+        state_id = ""
