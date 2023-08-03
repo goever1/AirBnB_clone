9e44233cabff4aac4b2425052d44d67f75473096
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-03 09:54:53 -0500

    reset city.py

--- models/city.py
+++ models/city.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from os import getenv
-from models.base_model import BaseModel, Base
-import models
-from sqlalchemy import Column, String, Integer, ForeignKey
-from sqlalchemy.orm import relationship
+from models.base_model import BaseModel
 
 
-class City(BaseModel, Base):
+class City(BaseModel):
     """ The city class, contains state ID and name """
-    __tablename__ = 'cities'
-    if getenv('if getenv('HBNB_TYPE_STORAGE') == 'db':
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-    else:
-        state_id = ""
-        name = ""
+    state_id = ""
+    name = ""
