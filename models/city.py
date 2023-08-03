4a11c9a87d02a71a7711a3dbc676ffe76d8e7ccb
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-03 15:33:15 -0500

    Update city.py

--- models/city.py
+++ models/city.py
@@ -2,9 +2,13 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
-
+from os
 class City(BaseModel):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
-    name = Column(String(128), nullable=False)
-    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
+    else:
+        name = ''
+        state_id = ''
