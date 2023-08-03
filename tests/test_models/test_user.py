923413f2a0798183451fd412f4a20c6fb50efd63
gaburon <japo000@hotmail.com>
2023-08-03 14:16:46 -0500

    sisa pero no

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
 """ """
+import models
+from os import getenv
+import unittest
 from tests.test_models.test_base_model import test_basemodel
 from models.user import User
+from sqlalchemy.exc import OperationalError
 
 
 class test_User(test_basemodel):
@@ -32,3 +36,19 @@ class test_User(test_basemodel):
         """ """
         new = self.value()
         self.assertNotEqual(type(new.password), int)
+
+    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') != 'db', "not supported")
+    def test_without_mandatory_arguments(self):
+        """Check for mandatory arguments """
+        new = self.value()
+        with self.assertRaises(OperationalError):
+            try:
+                new.save()
+            except Exception as error:
+                models.storage._DBStorage__session.rollback()
+                raise error
+
+    @unittest.skipIf(getenv('HBNB_TYPE_STORAGE') == 'db', "not supported")
+    def test_is_subclass(self):
+        """Check that State is a subclass of Basemodel"""
+        self.assertTrue(isinstance(self.user, User))
