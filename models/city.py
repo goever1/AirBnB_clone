9a372a4a1836b20bfbcc5f478bebce154dc8de95
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-03 10:39:41 -0500

    Update city.py

--- models/city.py
+++ models/city.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ = "cities"
+    state_id = Column(string(60), nullable=False, Foreignkey("states.id")
+    name = Column(string(128), nullable=False)
