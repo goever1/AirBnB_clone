fddba08e8cb4ec60d4b93b327be6a4b5203af81b
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-03 16:20:33 -0500

    Update place.py

--- models/place.py
+++ models/place.py
@@ -13,7 +13,7 @@ env = getenv('HBNB_TYPE_STORAGE')
 class Place(BaseModel, Base if (env == "db") else object):
     """ A place to stay """
     if env == 'db':
-        __tablename__ = 'places'
+        __tablename__ = "places"
         city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
         user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
         name = Column(String(128), nullable=False)
