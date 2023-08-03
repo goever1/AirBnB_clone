f92f68be8659d7fdb80415a130ae2b73d858d6ce
gaburon <japo000@hotmail.com>
2023-08-03 15:26:14 -0500

    fix

--- models/city.py
+++ models/city.py
@@ -8,7 +8,3 @@ class City(BaseModel):
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
-<<<<<<< HEAD
-    places = relationship("Place", backref="cities", cascade="delete")
-=======
->>>>>>> 4ff267333be7a5f325a76a9bf56474034877cd23
