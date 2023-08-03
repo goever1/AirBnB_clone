2fd7cfa44aebf6083dc9cf6cd3da3ab648968a91
gaburon <japo000@hotmail.com>
2023-08-03 15:16:02 -0500

    sisa

--- models/__init__.py
+++ models/__init__.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
 """This module instantiates an object of class FileStorage"""
-from models.user import User
-from models.state import State
-from models.review import Review
-from models.place import Place
-from models.city import City
-from models.base_model import BaseModel
-from models.amenity import Amenity
 from os import getenv
-"""for using getenv to determine with which kind of storage we work"""
 
-if getenv('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine import db_storage
-    storage = db_storage.DBStorage()
+
+env = getenv('HBNB_TYPE_STORAGE')
+if env == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+    storage.reload()
 else:
-    from models.engine import file_storage
-    storage = file_storage.FileStorage()
-
-classes = {"Amenity": Amenity, "BaseModel": BaseModel, "City": City,
-           "Place": Place, "Review": Review,
-           "State": State, "User": User}
-
-storage.reload()
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
+    storage.reload()
