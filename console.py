2fec72993b56baaa609185064fc5c1961c937280
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-03 12:08:00 -0500

    Update console.py

--- console.py
+++ console.py
@@ -115,7 +115,7 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
-         if not args:
+        if not args:
             print("** class name missing **")
             return
         args_list = args.split()
