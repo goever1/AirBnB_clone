640f5f53d2b097c26646b0f206db8703e30416c4
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-04 10:21:53 -0500

    Update city.py

--- models/city.py
+++ models/city.py
@@ -8,8 +8,17 @@ from os import getenv
 env = getenv('HBNB_TYPE_STORAGE')
 
 
-class City(BaseModel, Base):
+class City(BaseModel, Base if (env == "db") else object):
     """ The city class, contains state ID and name """
+   if env == "db":
+        __tablename__ = "cities"
+
+        name = Column(String(128), nullable=False)
+        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+        places = relationship("Place", backref="cities", cascade="delete")
+    else:
+        name = ""
+        state_id = ""
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
