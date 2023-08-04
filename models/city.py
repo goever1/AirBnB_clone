94503b3e190a6e8de1042029790483551d52a0ec
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-04 12:04:50 -0500

    Update city.py

--- models/city.py
+++ models/city.py
@@ -10,15 +10,6 @@ env = getenv('HBNB_TYPE_STORAGE')
 
 class City(BaseModel, Base if (env == "db") else object):
     """ The city class, contains state ID and name """
-   if env == "db"
-        __tablename__ = "cities"
-
-        name = Column(String(128), nullable=False)
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        places = relationship("Place", backref="cities", cascade="delete")
-    else:
-        name = ""
-        state_id = ""
     __tablename__ = "cities"
     name = Column(String(128), nullable=False)
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
