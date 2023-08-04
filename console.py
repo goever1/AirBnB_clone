54bc0a301e4dbbd9d8a76c5d2605c36acd63cbb5
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-04 16:06:34 -0500

    Update console.py

--- console.py
+++ console.py
@@ -223,7 +223,7 @@ class HBNBCommand(cmd.Cmd):
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
-            for k, v in storage.all().items():
+            for k, v in storage.all(HBNBCommand.classes[args]).items():
                 print_list.append(str(v))
 
         print(print_list)
