f98837824db82df743666e2b35b312f682a0e4b2
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-04 16:11:49 -0500

    Update state.py

--- models/state.py
+++ models/state.py
@@ -9,7 +9,6 @@ class State(BaseModel, Base):
     """ State class """
 
     __tablename__ = "states"
-
     name = Column(String(128), nullable=False)
     cities = relationship('City', backref='state', cascade='all, delete')
 
