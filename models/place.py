ffa5dd505e0df51d380c7513b446dfa122079a35
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-04 14:37:02 -0500

    Update place.py

--- models/place.py
+++ models/place.py
@@ -8,7 +8,10 @@ from sqlalchemy.orm import relationship
 from os import getenv
 import models
 
-place_amenity = Table("place_amenity", Base.metadata, extend_existing=True, Column('place_id', String(60), ForeignKey('places.id'), primary_key=True, nullable=False),
+place_amenity = Table("place_amenity", Base.metadata, extend_existing=True,
+                      Column('place_id', String(60), 
+                             ForeignKey('places.id'), 
+                             primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
                              ForeignKey('amenities.id'),
                              primary_key=True, nullable=False))
