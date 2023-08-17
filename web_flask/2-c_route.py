3e7634645073fa93eb5b5eafbd24ff91adff764d
goever1 <30449167+goever1@users.noreply.github.com>
2023-08-17 13:49:53 -0500

    Update 2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-"""
+'''
 A script that starts a Flask web application
-"""
+'''
 
 from flask import Flask
 
@@ -9,17 +9,14 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
-    """Function that returns a simple string"""
     return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Function that returns HBNB string"""
     return "HBNB"
 
 @app.route("c/<text>", strict_slashes=False)
 def c(text):
-    """Function that returns a C string"""
     return f"C {text.replace('_', ' ')}"
 
 if __name__ == '__main__':
