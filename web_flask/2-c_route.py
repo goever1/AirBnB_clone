cb575e8602961f66cf021ed1bd87c932c04917ba
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-17 13:37:17 -0500

    C is not fun

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -18,6 +18,8 @@ def hbnb():
 
 @app.route("c/<text>", strict_slashes=False)
 def c(text):
+    """Function that returns a C string"""
     return f"C {text.replace('_', ' ')}"
+
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
