a59bb580cb0e74a5b250ee483d7f89b084f4eac0
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-17 14:11:23 -0500

    hello Flask

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python3
-"""
-A script that starts a Flask web application
-"""
+'''Flask WSGI for multiple routes'''
 
 from flask import Flask
-from sys import argv
+
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
+    '''First function. Prints on /'''
     return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
+    '''Second function. Prints on /hbnb'''
     return "HBNB"
+
 
 @app.route("c/<text>", strict_slashes=False)
 def c(text):
+    '''Third function. Prints on /c/anything'''
     return f"C {text.replace('_', ' ')}"
 
-@app.route("/python", default={'text': "is cool"}, strict_slashes=False)
+
 @app.route("/python/<text>", strict_slashes=False)
 def pyton(text):
+    '''Fourth function. Prints on /python/anything'''
     return f"Python { text.replace('_', ' ')}"
+
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
