89f57ce89bfc07d624501f7c3b6bbed9afe078ec
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-17 14:20:37 -0500

    hello Flask

---
+++ web_flask/5-number_template.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+'''Flask WSGI for multiple routes'''
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    '''First function. Prints on /'''
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def just_hbnb():
+    '''Second function. Prints on /hbnb'''
+    return "HBNB"
+
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    '''Third function. Prints on /c/anything'''
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+@app.route("/python/<text>", strict_slashes=False)
+def python_is_cool(text='is cool'):
+    '''Fourth function. Prints on /python/anything'''
+    text = text.replace("_", " ")
+    return "Python {}".format(text)
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def only_int(n):
+    '''Fifth function. Prints on number if n is int'''
+    if type(n) == int:
+        return "{} is a number".format(n)
+
+
+@app.route("/number_template/<n>")
+def n_temp(n):
+    if type(n) == int:
+        return render_temp('5-number.html', title='HBNB', n=n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
