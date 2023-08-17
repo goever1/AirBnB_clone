73f18af74cfab59f4eacb84e0dfcd4334357ed21
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-17 13:55:47 -0500

    hello Flask

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -6,20 +6,25 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
-def hello():
-     '''Function that returns a simple string'''
-     return "Hello HBNB!"
+def hello_hbnb():
+    '''First function. Prints on /'''
+    return "Hello HBNB!"
+
 
 @app.route("/hbnb", strict_slashes=False)
-def hbnb():
-     '''Second function. Prints on /hbnb'''
-     return "HBNB"
+def just_hbnb():
+    '''Second function. Prints on /hbnb'''
+    return "HBNB"
 
-@app.route("c/<text>", strict_slashes=False)
-def c(text):
-     '''Third function. Prints on /c/anything'''
-     return f"C {text.replace('_', ' ')}"
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+@app.route("/c/<text>", strict_slashes=False)
+def c_is_fun(text):
+    '''Third function. Prints on /c/anything'''
+    text = text.replace("_", " ")
+    return "C {}".format(text)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
