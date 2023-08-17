82e7f992bf228bee1f3125ccfe9deba941248860
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-17 15:40:58 -0500

    hello Flask

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+'''Flask WSGI for multiple routes'''
+
+
+from flask import Flask, render_template
+from models import storage, storage.all(...)
+
+app = Flask(__name__)
+
+@app.teardown_appcontext
+def teardown(self):
+    '''Method to remove current SQLAlchemy Session'''
+    storage.close()
+
+
+@app.route("/states_list", strict_slashes=False)
+def disp_satates():
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
