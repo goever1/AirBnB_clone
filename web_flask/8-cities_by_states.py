e229d390e28328e08f46107638489abc823b0757
goever1 <jorgefernandoochoa1@hotmail.com>
2023-08-18 10:07:25 -0500

    hello Flask

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
-
 '''A script that starts a Flask web application'''
 
 
 from flask import Flask, render_template
 from models import storage
 from models.state import State
-
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def close(self)
+def close(self):
     '''It close the session'''
     storage.close()
 
 
 @app.route("/cities_by_states", strict_slashes=False)
-def cities():
+def cities_by_states():
     '''Shows the states and cities in an HTML page'''
-    states = storage.all(state)
-    return render_template('8-cities_by_states.html', )
+    states = storage.all(State)
+    return render_template('8-cities_by_states.html', states=states )
 
 
 if __name__ == '__main__':
